ccaca70aa28bdd3e4f2a9c6e46d76e3ff8653f88
Asad Dhamani <dhamaniasad@gmail.com>
2018-03-03 03:31:58 +0530

    Fix public page hashids issue

--- crestify/views/public.py
+++ crestify/views/public.py
@@ -5,6 +5,6 @@ from flask import render_template
 
 @app.route('/public/<string:bookmark_id>', methods=['GET'])
 def bookmark_public(bookmark_id):
-    bookmark_id = hashids.decode(bookmark_id)[0]
+    bookmark_id = hashids.decode(str(bookmark_id))[0]
     query = Bookmark.query.get(bookmark_id)
     return render_template("public/bookmark_share.html", bookmark=query)
