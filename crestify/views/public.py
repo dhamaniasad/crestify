1c5f32c12ad243ffbe144a4190158a928db3ed12
Asad Dhamani <dhamaniasad@gmail.com>
2020-11-12 13:01:58 +0530

    Add public bookmark route with int id
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- crestify/views/public.py
+++ crestify/views/public.py
@@ -1,6 +1,15 @@
 from crestify import app, hashids
 from crestify.models import Bookmark
-from flask import render_template
+from flask import render_template, abort
+
+
+@app.route('/pub/<int:bookmark_id>', methods=['GET'])
+def bookmark_share(bookmark_id):
+    if bookmark_id < 36000:  # When this was implemented, there were about those many bookmarks in the database
+        query = Bookmark.query.get(bookmark_id)
+        return render_template("public/bookmark_share.html", bookmark=query)
+    else:
+        return abort(403)
 
 
 @app.route('/public/<string:bookmark_id>', methods=['GET'])
