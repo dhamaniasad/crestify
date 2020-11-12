dd2a861d71f3edee340307eccb12e14d2aabc535
Asad Dhamani <dhamaniasad@gmail.com>
2020-11-12 13:11:14 +0530

    Update main.py file
    Signed-off-by: Asad Dhamani <dhamaniasad@gmail.com>

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from crestify.views.settings import *
 from crestify.views.site import *
 from crestify.views.apiservice import *
 from crestify.views.public import *
+from crestify.views.admin import *
 from crestify.tasks import *
 from crestify.signal_handlers import *
 from crestify.mail import Mail
@@ -13,4 +14,5 @@ from crestify import manager
 
 
 if __name__ == '__main__':
+    # db.create_all()
     manager.run()
